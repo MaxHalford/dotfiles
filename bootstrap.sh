4f232ec2660556047c82bb73a345d6d1f3f4f3be
Max Halford <maxhalford25@gmail.com>
2017-02-04 17:20:50 +0100

    Init

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,11 @@
-#!/usr/bin/env bash
+rsync --exclude ".git/" \
+	--exclude ".DS_Store" \
+	--exclude ".osx" \
+	--exclude "bootstrap.sh" \
+	--exclude "README.md" \
+	--exclude "LICENSE-MIT.txt" \
+	-avh --no-perms . ~;
 
-cd "$(dirname "${BASH_SOURCE}")";
-
-git pull origin master;
-
-function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . ~;
-	source ~/.bash_profile;
-}
-
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;
+for file in ~/.{aliases,bash_profile,exports,zshrc}; do
+    source $file
+done;
