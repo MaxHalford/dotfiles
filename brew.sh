0d8b8f14bcacd136550f10b7bce260549dd46a39
Max Halford <maxhalford25@gmail.com>
2017-03-06 20:16:01 +0100

    Update

--- brew.sh
+++ brew.sh
@@ -12,6 +12,8 @@ brew upgrade
 # Install necessary libraries
 brew install git
 brew install mysql
+brew install npm
+brew install ruby
 brew install Caskroom/cask/java
 
 # Remove outdated versions from the cellar.
