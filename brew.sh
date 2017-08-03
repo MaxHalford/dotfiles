701bbb758bb19825920a182cf35380cb08d3ee63
Max Halford <maxhalford25@gmail.com>
2017-08-03 23:13:34 +0200

    Edit stuff

--- brew.sh
+++ brew.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-# Install command-line tools using Homebrew.
+# Install command-line tools using Homebrew
 
-# Make sure we’re using the latest Homebrew.
+# Make sure we’re using the latest Homebrew
 brew update
 brew tap caskroom/cask
 
-# Upgrade any already-installed formulae.
+# Upgrade any already-installed formulae
 brew upgrade
 
 # Install necessary libraries
@@ -15,6 +15,10 @@ brew install mysql
 brew install npm
 brew install ruby
 brew install Caskroom/cask/java
+brew install zsh
 
-# Remove outdated versions from the cellar.
+# No formulas but needed
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Remove outdated versions from the cellar
 brew cleanup
